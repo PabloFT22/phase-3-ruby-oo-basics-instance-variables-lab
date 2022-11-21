e32cdc8e9b3e4fcffa60f1791901f1a96a96771a
Pablo Fuentes Tudela <pablo.fuentes.tudela@outlook.com>
2022-11-21 17:08:53 -0500

    commit tree eed193dec1ad45b9dbaab3f3435a10f650007a12

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
 
+    # 2 instance methods
+
+    def name=(dog_name)   #setter   
+        @this_dogs_name = dog_name
+    end
+
+    def name    #getter
+        @this_dogs_name
+    end
+
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+lassie.name
